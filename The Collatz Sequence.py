c7608d1efafb5d765a79bacab9ed092699429e8b
dabadee <74227807+hassaan253@users.noreply.github.com>
2021-11-04 20:39:55 +0500

    The Collatz Sequence
    The simplest possible math problem

---
+++ The Collatz Sequence.py
@@ -0,0 +1,14 @@
+# Write your code here
+def collatz(number):
+    number = int(number)
+    if number % 2 == 0:
+        return number //2
+    else:
+        return number * 3 +1
+
+print("Enter your number: ")
+num = input()
+while(num != 1):
+    num = collatz(num)
+    print(num)
+
