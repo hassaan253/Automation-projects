c714c3cf2a2fa848d058883035092068d48e15d8
dabadee <74227807+hassaan253@users.noreply.github.com>
2021-12-07 18:59:00 +0500

    Character picture grid
    project for chapter 4

---
+++ character picture grid.py
@@ -0,0 +1,18 @@
+# Character Picture Grid
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+#create two for loops
+#first loop through the columns
+for i in range(6):
+    for j in range(9):
+        #loop through rows
+        #print out the first column of each row
+        print(grid[j][i], end = "")
+    print("\n")
